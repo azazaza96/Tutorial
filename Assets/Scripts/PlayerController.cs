796a3fefe5e55a6e39dc4e7d6cd0d8df8832ea17
安住　瑠唯 <SDT2395GP0001@edu.sca.ac.jp>
2023-06-02 12:10:30 +0900

    6/2 (金）

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//UIを使えるようにする
 
 public class PlayerController : MonoBehaviour
 {
+    public int score = 0;
+    [SerializeField] Text scoreText;//スコア表示用Text
     private float horizontalInput;
     [SerializeField] private float speed;
     [SerializeField] private float xRange;
     [SerializeField] private GameObject projectilePrefab;//食べ物プレハブ（あとで複製）
+
+     void Start()
+    {
+        SetCountText(0);//初期化
+    }
+
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
@@ -30,6 +39,11 @@ public class PlayerController : MonoBehaviour
                             transform.position,
                             projectilePrefab.transform.rotation);
         }
-
+    }
+    //内部的なscoreをUI更新させる
+    public void SetCountText(int point)
+    {
+        score += point;//動物からもらったpointを自分のスコアに加算
+        scoreText.text = "Score:" + score.ToString();//【重要】UI更新
     }
 }
